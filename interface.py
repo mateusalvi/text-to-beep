6d5b72fb8083abfbe495839834c4d2cd77c121f8
Mateus Salvi <mateusalvi@hotmail.com>
2021-03-29 19:07:36 -0300

    Interface Update
    -Botões play e pause funcionam agora

--- interface.py
+++ interface.py
@@ -1,30 +1,41 @@
 import PySimpleGUI as sg
 
 class interface:
-
-    play_button = sg.Button('Tocar música',font='halvetica',disabled=False)
-    stop_button = sg.Button('Parar música',font='halvetica',disabled=True)
-    text_box    = sg.Multiline(default_text='',size=(75, 25),font='halvetica')
-    layout =[ [sg.Text (" ",size=(10, 1))],
+    sg.theme('Light Blue 2')                                                                                                    #Definição do tema à ser usado
+    play_button = sg.Button('Tocar música',font='halvetica',disabled=False,key='play_button')                                   #Estado inicial do botão play
+    stop_button = sg.Button('Parar música',font='halvetica',disabled=True,key='stop_button')                                    #Estado inicial do botão Stop
+    text_box    = sg.Multiline(default_text='',size=(75, 25),font='halvetica',key='text_box')                                   #Caixa de texto
+    layout =[ [sg.Text (" ",size=(10, 1))],                                                                                     #Tabela com os elementos da interface (linhas e colunas)
             [sg.Text (" ",size=(14, 1)), sg.Text("TEXT-TO-BEEP",justification='center',size=(20, 1),font=['halvetica',30]), ],
             [text_box, ],
             [sg.Text (" ",size=(10, 1))],
             [sg.Text (" ",size=(20, 2)), play_button, sg.Text(" ",size=(10, 2)), stop_button, ],
             [sg.Text (" ",size=(10, 1))], ]
-    window = sg.Window('Text-to-Beep', layout)
-    event, values = window.read()
+    window = sg.Window('Text-to-Beep', layout)                                                                                  #Cria a janela com os elementos acima
+    event = 0                                                                                                                   #Inicializa variavel que recebe os eventos da janela
+    values = 0                                                                                                                  #Outros valores usado para eventos
 
     def __init__(self):
         pass
 
+    #play() altera o botão de play para DESABILITADO e o de pausa para HABILITADO.
     def play(self):
-        interface.play_button.disabled = True
-        interface.stop_button.disabled = False
-
+        self.window['play_button'].update(disabled=True)
+        self.window['stop_button'].update(disabled=False)
+        self.event=0
+    #Complementar à def play(self).
     def stop(self):
-        interface.play_button.disabled = False
-        interface.stop_button.disabled = True
+        self.window['play_button'].update(disabled=False)
+        self.window['stop_button'].update(disabled=True)
+        self.event=0
 
-    def draw_and_update_window(self):
-        #event, values = interface.window.read()
-        pass
+    #update_window(self) checa para interações com a interface e armazena em EVENT e VALUES. Em seguida é feita a alteração necessária na interface.
+    def update_window(self):
+        self.event, self.values = self.window.read()
+        
+        if(self.event == "play_button"):
+            interface.play(self)
+        elif(self.event == "stop_button"):
+            interface.stop(self)
+        else:
+            pass
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import interface as interface
 
 def main():
-    
+    should_close = False
     window = interface.interface
-    print(window.event)
+   
+    while (should_close != True):
+        window.update_window(window)    #Checa e atualiza eventos e entradas na interface
+        
     
 if __name__ == "__main__":
     main()
