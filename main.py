e199f1c0fffc1adf11ad8faad94801f86ff4b7ab
Mateus Salvi <mateusalvi@hotmail.com>
2021-03-29 20:34:55 -0300

    Update main.py
    -Bugfix ao fechar a janela

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import interface as interface
 
 def main():
-    should_close = False
     window = interface.interface
    
-    while (should_close != True):
-        window.update_window(window)    #Checa e atualiza eventos e entradas na interface
-        
+    while True:
+        window.update_window(window)    #!!Checa e atualiza eventos e entradas na interface!!
+        if (window.event == interface.sg.WIN_CLOSED): #!!Fecha o programa se o usuário fechar a janela!!
+            break
     
 if __name__ == "__main__":
     main()
