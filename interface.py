75a03737b425992ff7e3169c954b773be0be928d
Mateus Salvi <mateusalvi@hotmail.com>
2021-05-12 20:07:22 -0300

    Update na interface
    Adicionado:
    -Retorno de texto para a caixa de input
    -Carregamento de arquivo TXT para a caixa de input
    -Botões para as funcionalidades de carregar e exportar para MIDI

--- interface.py
+++ interface.py
@@ -2,14 +2,19 @@ import PySimpleGUI as sg
 
 class interface:
     sg.theme('Light Blue 2')                                                                                                    #Definição do tema à ser usado
-    play_button = sg.Button('Tocar música',font='Helvetica',disabled=False,key='play_button')                                   #Estado inicial do botão play
-    stop_button = sg.Button('Parar música',font='Helvetica',disabled=True,key='stop_button')                                    #Estado inicial do botão Stop
-    text_box    = sg.Multiline(default_text='',size=(75, 25),font='Helvetica',key='text_box')                                   #Caixa de texto
-    layout =[ [sg.Text (" ",size=(10, 1))],                                                                                     #Tabela com os elementos da interface (linhas e colunas)
+    #Definição dos botões
+    play_button   = sg.Button('Tocar música', font='Helvetica', disabled=False, key='play_button')                                 #Estado inicial do botão play
+    stop_button   = sg.Button('Parar música', font='Helvetica', disabled=True, key='stop_button')                                  #Estado inicial do botão Stop
+    select_button = sg.FileBrowse('Carregar arquivo', font='Helvetica', key='select_button', file_types=(("Text Files", "*.txt"), ))
+    export_button = sg.Button('Exportar MIDI', font='Helvetica', key='export_button')
+    text_box      = sg.Multiline(default_text='', size=(75, 25), font='Helvetica', key='text_box')                                 #Caixa de texto
+    #Definição da interface
+    layout =[[sg.Text (" ",size=(10, 1))],                                                                                     #Tabela com os elementos da interface (linhas e colunas)
             [sg.Text (" ",size=(14, 1)), sg.Text("TEXT-TO-BEEP",justification='center',size=(20, 1),font=['Helvetica',30]), ],
-            [text_box, ],
+            [text_box,],
             [sg.Text (" ",size=(10, 1))],
-            [sg.Text (" ",size=(20, 2)), play_button, sg.Text(" ",size=(10, 2)), stop_button, ],
+            [sg.Text (" ",size=(20, 2)), play_button, sg.Text(" ",size=(10, 2)), stop_button],
+            [sg.Text (" ",size=(20, 2)),export_button, sg.Input(key='file_selected', visible=False, enable_events=True), select_button],
             [sg.Text (" ",size=(10, 1))], ]
     window = sg.Window('Text-to-Beep', layout)                                                                                  #Cria a janela com os elementos acima
     event = 0                                                                                                                   #Inicializa variavel que recebe os eventos da janela
@@ -18,18 +23,37 @@ class interface:
     def __init__(self):
         pass
 
-    #play() altera o botão de play para DESABILITADO e o de pausa para HABILITADO.
+    #play() altera o botão de play para DESABILITADO e os demais HABILITADO. Significando que o software esta reproduzindo a musica e nao deve receber nenhum outro input alem de STOP
     def play(self):
         self.window['play_button'].update(disabled=True)
+        self.window['select_button'].update(disabled=True)
+        self.window['export_button'].update(disabled=True)
         self.window['stop_button'].update(disabled=False)
-        self.event=0
+
     #Complementar à def play(self).
     def stop(self):
         self.window['play_button'].update(disabled=False)
+        self.window['select_button'].update(disabled=False)
+        self.window['export_button'].update(disabled=False)
         self.window['stop_button'].update(disabled=True)
-        self.event=0
 
-    #update_window(self) checa para interações com a interface e armazena em EVENT e VALUES. Em seguida é feita a alteração necessária na interface.
+    #Carrega um arquivo .txt e passa ele para a caixa de texto
+    def openFile(self):
+        file_path = self.values['file_selected']
+        if (file_path != ''):
+            file = open(file_path, "r")
+            self.text_box.print(file.read())
+            print('Debug: Arquivo Carregado')
+        else:
+            print('Debug: Problema ao carregar arquivo')
+
+        
+
+    #Metodo que retorna o texto que está na caixa de texto
+    def returnText (self):
+        return self.text_box.get()
+
+    #update_window(self) checa para interações com a interface e armazena em EVENT e VALUES. Em seguida é feita a alteração visual necessária na interface.
     def update_window(self):
         self.event, self.values = self.window.read()
         
--- main.py
+++ main.py
@@ -4,9 +4,30 @@ def main():
     window = interface.interface
    
     while True:
-        window.update_window(window)    #!!Checa e atualiza eventos e entradas na interface!!
-        if (window.event == interface.sg.WIN_CLOSED): #!!Fecha o programa se o usuário fechar a janela!!
-            break
+        window.update_window(window)#!!Checa e atualiza eventos e entradas na interface!!
+        
+
+        if (window.event == "play_button"):
+            print('Debug: Tocando a musica')
+            input_text = window.returnText(window)
+            #chamar o interpretador(passar texto)
+
+        elif (window.event == "stop_button"):
+            print('Debug: Parou a música')
+            #parar a reprodução
+
+        elif (window.event == "file_selected"): #Ao se escolher um arquivo, passa ele para a caixa de texto
+            print('Debug: Carregar arquivo de texto')
+            window.openFile(window)
+        
+        elif (window.event == "export_button"):
+            print('Debug: Exportar para MIDI')
+            input_text = window.returnText(window)
+            #chamar a função exportar(texto)
     
+        elif (window.event == interface.sg.WIN_CLOSED):#!!Fecha o programa se o usuário fechar a janela!!
+            break
+        
+
 if __name__ == "__main__":
     main()
