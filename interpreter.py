23ac858804a1bf8339f08862f3c5a60d6d424749
Giovani da Silva <geeowdk@gmail.com>
2021-05-16 14:46:25 -0300

    Update interpreter.py
    criado método para criar objeto music

--- interpreter.py
+++ interpreter.py
@@ -1,118 +1,113 @@
 from sound import *
-import os #pra salvar o arquivo temporario na mesma pasta
+import os #usado em operações de diretórios e afins
 
 
 
-def interpretador(input_text,nome):
+def interpreter(input_text):
     midi_info = MIDIInfo()
-    lista_instrumentos = list(midi_info.getMidiInstruments())
-    l = input_text
-    x=0 #ponteiro da lista
-    nota = 'A' #nota inicial
-    bpm = 100 #bpm inicial
-    volume = 100
-    instrumento = 0 #instrumento inicial
-    oitava = 1
-    lista = []
-    numeros = ['1','2','3','4','5','6','7','8','9','0']
-    listaNotas = ['A','B','C','D','E','F','G']
-    #tururu = Music(lista, volume, bpm, "Tururu")
-    #musica = Music(lista,volume,100,"TESTE OI")
-    #l="ZZBZGZDZZZBZGZDZZZBZGZDCDZZZBZGZDZZZBZGZDZZZBZGZDCDZ"
-
-    volume = 100
-    bpm = 100
-    #nome = "teste interpretador"
-
-    musica = Music(l,volume,bpm,nome)
-
-
-    while x < len(l):
-
-    #===========================================INSTRUMENTOS==============================
-        if l[x] == "!":
-            #instrumento = "agogô"
-            instrumento = 113
+    instruments_list = list(midi_info.getMidiInstruments())
+    input_text = input_text
+    x=0 #ponteiro da string input_text, irá percorrer no while
+    note = 'A' #note inicial
+    instrument = 0 #instrument inicial
+    octave = 5  #octave standard
+    song_list = []
+    list_of_numbers = ['1','2','3','4','5','6','7','8','9','0']
+    list_of_notes = ['A','B','C','D','E','F','G']
+    while x < len(input_text):
+    #===========================================instrumentS==============================
+        if input_text[x] == "!":
+            #instrument = "agogô"
+            instrument = 113
             x = x+1
-        elif l[x] == "\n":
-            #instrumento = "tubular bells"
-            instrumento = 14
+        elif input_text[x] == "\n":
+            #instrument = "tubular bells"
+            instrument = 14
             x = x+1
-        elif l[x] == ";":
-            #instrumento = "pan flute"
-            instrumento = 75
+        elif input_text[x] == ";":
+            #instrument = "pan flute"
+            instrument = 75
             x = x+1
-        elif l[x] == ",":
-            #instrumento = "church organ"
-            instrumento = 19
+        elif input_text[x] == ",":
+            #instrument = "church organ"
+            instrument = 19
             x = x+1
-        elif l[x] == "i" or l[x] == "I" or l[x] == "o" or l[x] == "O" or l[x] == "u" or l[x] == "U":
-            #instrumento = "harpsichord"
-            instrumento = 6
+        elif input_text[x] == "i" or input_text[x] == "I" or input_text[x] == "o" or input_text[x] == "O" or input_text[x] == "u" or input_text[x] == "U":
+            #instrument = "harpsichord"
+            instrument = 6
             x = x+1
 
-    #===========================================NOTAS==============================
-        elif l[x] in listaNotas:
-            nota = l[x]
+    #===========================================noteS==============================
+        elif input_text[x] in list_of_notes:
+            note = input_text[x]
             x=x+1
     #===========================================VOLUME==============================
-        elif l[x] == '-':
-            nota = '-'
+        elif input_text[x] == '-':
+            note = '-'
             x = x + 1
-        elif l[x] == " ":  #caso do espaço, dobra o volume
-            nota = " "
+        elif input_text[x] == " ":  #caso do espaço, dobra o volume
+            note = " "
             x=x+1
-    #===========================================OITAVAS==============================
-        elif l[x] == "?":
-            oitava = oitava+1
+    #===========================================octaveS==============================
+        elif input_text[x] == "?":
+            octave = octave+1
             x = x+1
-    #===========================================TROCA DE INSTRUMENTO POR NÚMERO==============================
-        elif l[x] in numeros:   #numeros é a lista de 0,1,2,3...
-            instrumento = instrumento + int(l[x])
+    #===========================================TROCA DE instrument POR NÚMERO==============================
+        elif input_text[x] in list_of_numbers:   #list_of_numbers é a lista de 0,1,2,3...
+            instrument = instrument + int(input_text[x])
             x = x+1
     ##===========================================CONSOANTE / OUTROS CARACTÉRES==============================
         else:
-            if l[x-1] in listaNotas:
-                nota = l[x-1]
+            if input_text[x-1] in list_of_notes:
+                note = input_text[x-1]
                 x = x + 1
             else:
-                nota = "-"
+                note = "-"
                 x = x+1
 
-        #print("instrumento: " + str(instrumento)) #DEBUG
-        lista.append((nota,oitava,lista_instrumentos[instrumento]))
-    #print(lista)
-    musica.setSounds(lista)
-    return musica
+        #print("instrument: " + str(instrument)) #DEBUG
+        song_list.append((note,octave,instruments_list[instrument]))
+
+    return song_list
+
+def create_music_object(input_text,name):
+    song_list = interpreter(input_text)
+    volume_standard = 100
+    bpm_standard = 5
+    obj_music_object = Music(song_list,volume_standard,bpm_standard,name)
+    return obj_music_object
+
+
+
 
 def play(input_text):
-    musica=interpretador(input_text,"\\temp")
+    music_object=create_music_object(input_text,"\\temp")
     rec = Recorder()
     path = os.getcwd()+"\\temp.mid"  #vai salvar na mesma pasta o qual foi executado
-    rec.recordMusic(musica,path)
+    rec.recordMusic(music_object,path)
     return
 
 def save(input_text,path):
 
-    nome= os.path.basename(path)
+    name= os.path.basename(path)
 
-    print(nome)
-    musica=interpretador(input_text,nome)
+    print(name)
+    music_object=create_music_object(input_text,name)
     rec = Recorder()
-    rec.recordMusic(musica,path)
+    rec.recordMusic(music_object,path)
     return
 
 
 
 
 '''
-Dá pra pegar o dicionário que contém todos os instrumentos e converter pra uma lista,
+Dá pra pegar o dicionário que contém todos os instruments e converter pra uma lista,
 assim o código é o próprio índice dele. Exemplo:
 
 midi_info = MIDIInfo()
-lista_instrumentos = list(midi_info.getMidiInstruments())
-print(lista_instrumentos[6]) #harpsichord
-print(lista_instrumentos[14]) #tubular bells
+instruments_list = list(midi_info.getMidiInstruments())
+print(instruments_list[6]) #harpsichord
+print(instruments_list[14]) #tubular bells
 '''
 
 
@@ -122,5 +117,5 @@ ZZBZGZDZZZBZGZDZZZBZGZDCDZZZBZGZDZZZBZGZDZZZBZGZDCDZ
 
 volume = 100
 bpm = 100
-nome = "teste interpretador"
+name = "teste interpreter"
 '''
